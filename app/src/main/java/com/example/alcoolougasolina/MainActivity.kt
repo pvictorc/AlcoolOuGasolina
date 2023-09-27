4b47cc7ad3097dab592bea2b56311d8f0d328be4
Victor <pvictorc@gmail.com>
2023-09-27 14:53:34 -0300

    Projeto rodando e pequenas correcoes

--- app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
+++ app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
@@ -2,15 +2,20 @@ package com.example.alcoolougasolina
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     var percentual:Double = 0.7
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState != null) {
+            percentual = savedInstanceState.getDouble("percentual")
+        }
         Log.d("PDM23","No onCreate, $percentual")
 
         val btCalc: Button = findViewById(R.id.btCalcular)
@@ -20,9 +25,14 @@ class MainActivity : AppCompatActivity() {
             Log.d("PDM23","No btCalcular, $percentual")
         })
     }
+
 override fun onResume(){
+    // todo Altere o app para recuperar o valor de percentual
     super.onResume()
-    Log.d("PDM23","No onResume, $percentual")
+    Log.d("PDM23","No onResume, percentual $percentual")
+    val edAlcool:EditText = findViewById(R.id.edAlcool)
+    var valorAlcool = edAlcool.text.toString()
+    Log.d("PDM23", "No onResume, alcool $valorAlcool" )
 }
 override fun onStart(){
     super.onStart()
@@ -37,7 +47,13 @@ override fun onStop(){
     Log.d("PDM23","No onResume")
 }
 override fun onDestroy(){
+
     super.onDestroy()
     Log.d("PDM23","No onResume")
 }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        outState.putDouble("percentual",percentual)
+        super.onSaveInstanceState(outState, outPersistentState)
+    }
 }
